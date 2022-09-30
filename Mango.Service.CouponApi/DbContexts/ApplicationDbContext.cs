820f4ee5f43637c96fe750087e992c17d7f68338
Sergey Ivanchenkov <surge001@hotmail.com>
2022-09-29 20:26:19 -0400

    CouponApi started.

---
+++ Mango.Service.CouponApi/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponApi.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApplicationDbContext"/> class.
+        /// </summary>
+        /// <param name="options">Options</param>
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        #endregion
+
+        ///// <summary>
+        ///// Gets or sets Products
+        ///// </summary>
+        //public DbSet<Product> Products { get; set; }
+        //public DbSet<CartHeader> CartHeaders { get; set; }
+        //public DbSet<CartDetails> CartDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
---
+++ Mango.Service.CouponApi/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+namespace Mango.Services.CouponApi
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                //config.CreateMap<CartHeader, CartHeaderDto>().ReverseMap();
+                //config.CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+                //config.CreateMap<Product, ProductDto>().ReverseMap();
+                //config.CreateMap<Cart, CartDto>().ReverseMap();
+            });
+            return mappingConfig;
+        }
+    }
+}
